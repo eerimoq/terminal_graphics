b233d091128832e04a1302393da60ef9d9ed40d6
Erik Moqvist <erik.moqvist@gmail.com>
2022-03-26 16:39:18 +0100

    Early files.

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import re
+
+
+def find_version():
+    return re.search(r"^__version__ = '(.*)'$",
+                     open('termimage/version.py', 'r').read(),
+                     re.MULTILINE).group(1)
+
+setup(name='termimage',
+      version=find_version(),
+      description=('Images in the terminal.'),
+      long_description=open('README.rst', 'r').read(),
+      author='Erik Moqvist',
+      author_email='erik.moqvist@gmail.com',
+      license='MIT',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+      ],
+      keywords=['terminal', 'image'],
+      url='https://github.com/eerimoq/termimage',
+      py_modules=['termimage'],
+      install_requires=[
+      ],
+      python_requires='>=3.6',
+      test_suite="tests",
+      entry_points = {
+          'console_scripts': ['termimage=termimage:_main']
+      })
