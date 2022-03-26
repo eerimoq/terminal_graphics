9e92719c31189897ab49b86dcfaa1dc44ca4285a
Erik Moqvist <erik.moqvist@gmail.com>
2022-03-26 19:09:37 +0100

    More of everything.

--- terminal_graphics/__init__.py
+++ terminal_graphics/__init__.py
@@ -1,8 +1,19 @@
+import argparse
 import sys
 
 from . import kitty
 
 
 def _main():
-    kitty.write_png(open(sys.argv[1], 'rb').read())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--size',
+                        default='0x0',
+                        help='Width and height in cells (default: 0x0).')
+    parser.add_argument('file')
+    args = parser.parse_args()
+
+    with open(args.file, 'rb') as fin:
+        kitty.write_png(fin.read(),
+                        size=tuple([int(v) for v in args.size.split('x')]))
+
     print()
--- terminal_graphics/kitty.py
+++ terminal_graphics/kitty.py
@@ -2,8 +2,7 @@ import sys
 from base64 import b64encode
 
 
-def _serialize_gr_command(**cmd):
-    payload = cmd.pop('payload', None)
+def _serialize_gr_command(payload, **cmd):
     cmd = ','.join(f'{k}={v}' for k, v in cmd.items())
     ans = []
     ans.append(b'\033_G')
@@ -18,18 +17,18 @@ def _serialize_gr_command(**cmd):
     return b''.join(ans)
 
 
-def _write_chunked(fout, **cmd):
-    data = b64encode(cmd.pop('data'))
+def _write_chunked(fout, data, **cmd):
+    data = b64encode(data)
 
     while data:
         chunk, data = data[:4096], data[4096:]
         m = 1 if data else 0
-        fout.write(_serialize_gr_command(payload=chunk, m=m, **cmd))
+        fout.write(_serialize_gr_command(chunk, m=m, **cmd))
         cmd.clear()
 
 
-def write_png(image, fout=None):
+def write_png(image, fout=None, size=(0, 0)):
     if fout is None:
         fout = sys.stdout.buffer
 
-    _write_chunked(fout, a='T', f=100, data=image)
+    _write_chunked(fout, image, a='T', f=100, c=size[0], r=size[1])
