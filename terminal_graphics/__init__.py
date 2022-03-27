34bd0302a8f9d77f552ead52adbe7a4881da9933
Erik Moqvist <erik.moqvist@gmail.com>
2022-03-27 10:13:48 +0200

    More stuff.

--- terminal_graphics/__init__.py
+++ terminal_graphics/__init__.py
@@ -7,20 +7,25 @@ from PIL import Image
 from . import kitty
 
 
+def _file_to_png_data(file):
+    image = BytesIO()
+    Image.open(file).save(image, 'png')
+
+    return image.getvalue()
+
+
 def _main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--size',
-                        default='0x0',
-                        help='Width and height in cells (default: 0x0).')
+    parser.add_argument('-s', '--size', help='Width and height in cells.')
     parser.add_argument('files', nargs="+")
     args = parser.parse_args()
 
-    for file in args.files:
-        image = BytesIO()
-        Image.open(file).save(image, 'png')
-        kitty.write_png(image.getvalue(),
-                        fout=sys.stdout.buffer,
-                        size=tuple([int(v) for v in args.size.split('x')]))
+    if args.size is not None:
+        size = tuple([int(v) for v in args.size.split('x')])
+    else:
+        size = None
 
+    for file in args.files:
+        kitty.write_png(_file_to_png_data(file), sys.stdout.buffer, size)
         sys.stdout.buffer.write(b'\n')
         sys.stdout.buffer.flush()
