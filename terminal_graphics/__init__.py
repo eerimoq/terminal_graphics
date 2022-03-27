7a9b48e62ba6017bf9e0f3a82eac4927a06a9e0e
Erik Moqvist <erik.moqvist@gmail.com>
2022-03-27 11:27:51 +0200

    Pad ratio.

--- terminal_graphics/__init__.py
+++ terminal_graphics/__init__.py
@@ -5,18 +5,28 @@ from io import BytesIO
 from PIL import Image
 
 from . import kitty
+from .utils import pad_ratio
 
 
-def _file_to_png_data(file):
-    image = BytesIO()
-    Image.open(file).save(image, 'png')
+def _file_to_png_data(file, size, fill):
+    image = Image.open(file)
 
-    return image.getvalue()
+    if size is not None:
+        if not fill:
+            image = pad_ratio(image, size)
+
+    output = BytesIO()
+    image.save(output, 'png', compress_level=1)
+
+    return output.getvalue()
 
 
 def _main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--size', help='Width and height in cells.')
+    parser.add_argument('--fill',
+                        action='store_true',
+                        help='Stretch or squish to fill given size.')
     parser.add_argument('files', nargs="+")
     args = parser.parse_args()
 
@@ -26,6 +36,8 @@ def _main():
         size = None
 
     for file in args.files:
-        kitty.write_png(_file_to_png_data(file), sys.stdout.buffer, size)
+        kitty.write_png(_file_to_png_data(file, size, args.fill),
+                        sys.stdout.buffer,
+                        size)
         sys.stdout.buffer.write(b'\n')
         sys.stdout.buffer.flush()
--- terminal_graphics/utils.py
+++ terminal_graphics/utils.py
@@ -1,3 +1,6 @@
+from PIL.ImageOps import pad
+
+
 def split_image_horizontally(image, number_of_images):
     """Split given image horizontally into given number of images.
 
@@ -7,3 +10,21 @@ def split_image_horizontally(image, number_of_images):
 
     for y in range(0, image.height, height):
         yield image.crop((0, y, image.width, y + height))
+
+
+def pad_ratio(image, size):
+    """Pad to fit given size. Keeps aspect ratio.
+
+    """
+
+    image_ratio = image.width / image.height
+    size_ratio = size[0] / size[1] / 2
+
+    if image_ratio < size_ratio:
+        width = int(image.height * size_ratio)
+        height = image.height
+    else:
+        width = image.width
+        height = int(image.width / size_ratio)
+
+    return pad(image, (width, height))
