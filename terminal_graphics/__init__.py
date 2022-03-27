c9395f916cf308e1c654b699a54be993b7ed4595
Erik Moqvist <erik.moqvist@gmail.com>
2022-03-27 21:18:31 +0200

    Scaling.

--- terminal_graphics/__init__.py
+++ terminal_graphics/__init__.py
@@ -1,39 +1,31 @@
 import argparse
 import sys
-from io import BytesIO
 
 from PIL import Image
-import numpy
+from PIL.ImageOps import scale as scale_image
 
 from . import kitty
-from .utils import pad_ratio
 
 
 def _main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--size', help='Width and height in cells.')
-    parser.add_argument('--fill',
-                        action='store_true',
-                        help='Stretch or squish to fill given size.')
+    parser.add_argument('-s', '--scale',
+                        type=float,
+                        help='Image size scale factor.')
     parser.add_argument('files', nargs="+")
     args = parser.parse_args()
 
-    if args.size is not None:
-        size = tuple([int(v) for v in args.size.split('x')])
-    else:
-        size = None
-
     for file in args.files:
-        write_file(file, sys.stdout.buffer, size, args.fill)
+        write_file(file, sys.stdout.buffer, args.scale)
         sys.stdout.buffer.write(b'\n')
 
 
-def write(image, fout, size=None, fill=False, protocol=None):
-    if size is not None and not fill:
-        image = pad_ratio(image, size)
+def write(image, fout, scale=None, protocol=None):
+    if scale is not None:
+        image = scale_image(image, scale)
 
-    kitty.write(image, fout, size)
+    kitty.write(image, fout)
 
 
-def write_file(path, fout, size=None, fill=False, protocol=None):
-    write(Image.open(path), fout, size, fill, protocol)
+def write_file(path, fout, scale=None, protocol=None):
+    write(Image.open(path), fout, scale, protocol)
