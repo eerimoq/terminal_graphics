7833b9f3c5bec70180110051af0bdbf22ec9bf07
Erik Moqvist <erik.moqvist@gmail.com>
2022-03-28 18:36:14 +0200

    Cleanup.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,9 +9,7 @@ class KittyTest(TestCase):
 
     def test_split_image_horizontally(self):
         image = Image.open('tests/files/test_split_image_horizontally/diagonal.png')
-
         images = list(utils.split_image_horizontally(image, 3))
-
         self.assertEqual(len(images), 3)
         os.makedirs('tests/build/test_split_image_horizontally', exist_ok=True)
 
