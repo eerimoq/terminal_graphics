afaf8ad71cde48c683a6a6d3600b9d8db33753b3
Erik Moqvist <erik.moqvist@gmail.com>
2022-03-29 20:46:22 +0200

    Subparsers.

--- terminal_graphics/__init__.py
+++ terminal_graphics/__init__.py
@@ -8,18 +8,7 @@ from . import kitty
 from .utils import pad_ratio
 
 
-def _main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--scale',
-                        type=float,
-                        help='Image size scale factor.')
-    parser.add_argument('--size', help='Width and height in cells.')
-    parser.add_argument('--fill',
-                        action='store_true',
-                        help='Stretch or squish to fill given size.')
-    parser.add_argument('files', nargs="+")
-    args = parser.parse_args()
-
+def _do_show(args):
     if args.size is not None:
         size = tuple([int(v) for v in args.size.split('x')])
     else:
@@ -28,6 +17,40 @@ def _main():
     for file in args.files:
         write_file(file, sys.stdout.buffer, args.scale, size, args.fill)
         sys.stdout.buffer.write(b'\n')
+
+
+def _do_info(args):
+    print(get_terminal_info())
+
+
+def _main():
+    parser = argparse.ArgumentParser()
+
+    # Workaround to make the subparser required in Python 3.
+    subparsers = parser.add_subparsers(title='subcommands',
+                                       dest='subcommand')
+    subparsers.required = True
+
+    subparser = subparsers.add_parser('show')
+    subparser.add_argument('--scale',
+                           type=float,
+                           help='Image size scale factor.')
+    subparser.add_argument('--size', help='Width and height in cells.')
+    subparser.add_argument('--fill',
+                           action='store_true',
+                           help='Stretch or squish to fill given size.')
+    subparser.add_argument('files', nargs="+")
+    subparser.set_defaults(func=_do_show)
+
+    subparser = subparsers.add_parser('info')
+    subparser.set_defaults(func=_do_info)
+
+    args = parser.parse_args()
+
+    try:
+        args.func(args)
+    except BaseException as e:
+        sys.exit('error: ' + str(e))
 
 
 def write(image, fout, scale=None, size=None, fill=None, protocol=None):
